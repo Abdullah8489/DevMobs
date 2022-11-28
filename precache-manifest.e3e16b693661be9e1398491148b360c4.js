329a0bf187104c8ff8c5939993ee7b215441fa17
Abdullah <abdullahsheikh207@gmail.com>
2022-11-28 23:26:07 +0500

    Updates

---
+++ precache-manifest.e3e16b693661be9e1398491148b360c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f3cf25acf98b1f7584f",
+    "url": "/Abdullah8489/DevMobs.git/static/css/main.1c08301d.chunk.css"
+  },
+  {
+    "revision": "2f3cf25acf98b1f7584f",
+    "url": "/Abdullah8489/DevMobs.git/static/js/main.2f3cf25a.chunk.js"
+  },
+  {
+    "revision": "c2812201fb71a1c0c9cb",
+    "url": "/Abdullah8489/DevMobs.git/static/js/1.c2812201.chunk.js"
+  },
+  {
+    "revision": "00d16ce6f55bdda70b26",
+    "url": "/Abdullah8489/DevMobs.git/static/js/runtime~main.00d16ce6.js"
+  },
+  {
+    "revision": "a9a171eafaab492d606f51a2abb03252",
+    "url": "/Abdullah8489/DevMobs.git/index.html"
+  }
+];
